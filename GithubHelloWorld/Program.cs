05f68dd80f2de0ea98b6d52e5d59a30198a1cf6c
Manuel Reigner <manuel.reigner@oebb.at>
2025-01-13 08:02:26 +0100

    Projektdateien hinzufügen.

---
+++ GithubHelloWorld/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GithubHelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
